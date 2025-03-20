a9395fa20ceb6a8ff0d7b4c6117e16ae20dadd13
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-03-20 20:32:15 +0300

    leetcode(ll): palindrome linked list

---
+++ LeetCode/python/0234.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode | None) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # get second half reversed
+        prev = None
+        curr = slow  # middle
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+
+        # compare by 2 pointers from head and reversed second half
+        while head and prev:
+            if head.val != prev.val:
+                return False
+            head = head.next
+            prev = prev.next
+
+        return True
